c1cd5cea122366ea89c4315a582b0e79a20a6f5f
Vogeel <vitorvogel12@gmail.com>
2022-03-08 15:18:19 -0300

    Add project files.

---
+++ OrientacaoObjeto4.ConsoleApp/Class1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrientacaoObjeto4.ConsoleApp
+{
+    internal class Carro
+    {
+        public double KmInicial;
+        public double KmFinal;
+        public double consumo;
+        public double KmPorLitro()
+        {
+            double ValorFinal = (KmFinal - KmInicial) / consumo;
+            return ValorFinal;
+        }
+    }
+}
---
+++ OrientacaoObjeto4.ConsoleApp/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace OrientacaoObjeto4.ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Carro MediaPorLitro = new Carro();
+            MediaPorLitro.KmInicial = 1000;
+            MediaPorLitro.KmFinal = 1500;
+            MediaPorLitro.consumo = 450;
+
+            Console.WriteLine($"O veículo fez uma media de {MediaPorLitro.KmPorLitro():F2} Km/L");
+            Console.ReadKey();
+        }
+    }
+}
